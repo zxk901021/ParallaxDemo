54cbc231d29be287530c9d8a73a75913cf3ec0d1
zxk901021 <447360581@qq.com>
2015-05-07 16:57:38 +0800

    Fragment 导入包错误

--- app/src/main/java/com/example/zhy_9/parallaxdemo/BaseGuideFragment.java
+++ app/src/main/java/com/example/zhy_9/parallaxdemo/BaseGuideFragment.java
@@ -1,11 +1,12 @@
 package com.example.zhy_9.parallaxdemo;
 
-import android.app.Fragment;
+
+import android.support.v4.app.Fragment;
 
 /**
  * Created by ZHY_9 on 2015/5/7.
  */
-public abstract class BaseGuideFragment extends Fragment{
+public abstract class BaseGuideFragment extends Fragment {
     public abstract int[] getChildViewIds();
     public abstract int getRootViewId();
 }
